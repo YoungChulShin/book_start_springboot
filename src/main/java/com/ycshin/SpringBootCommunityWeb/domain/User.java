5e6d247c07938fd1c5e0a4f9f4675bedc23e458d
YoungChulShin <go1323@gmail.com>
2020-06-30 22:23:16 +0900

    OAuth 구현 진행 중

--- src/main/java/com/ycshin/SpringBootCommunityWeb/domain/User.java
+++ src/main/java/com/ycshin/SpringBootCommunityWeb/domain/User.java
@@ -1,13 +1,11 @@
 package com.ycshin.SpringBootCommunityWeb.domain;
 
+import com.ycshin.SpringBootCommunityWeb.domain.enums.SocialType;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Getter
@@ -25,15 +23,23 @@ public class User {
 
     private String email;
 
+    private String principal;
+
+    @Enumerated(EnumType.STRING)
+    private SocialType socialType;
+
     private LocalDateTime createdDate;
 
     private LocalDateTime updatedDate;
 
     @Builder
-    public User(String name, String password, String email, LocalDateTime createdDate, LocalDateTime updatedDate) {
+    public User(String name, String password, String email, String principal,
+                SocialType socialType, LocalDateTime createdDate, LocalDateTime updatedDate) {
         this.name = name;
         this.password = password;
         this.email = email;
+        this.principal = principal;
+        this.socialType = socialType;
         this.createdDate = createdDate;
         this.updatedDate = updatedDate;
     }
